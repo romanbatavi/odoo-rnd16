75433bc48435568da9e8fbfcb9f44e8af5ab7e5a
Roman Batavi <romanbatavi98@gmail.com>
2023-06-09 10:19:32 +0700

    [DONE]rm_base_rnd

--- rm_base_rnd/models/warung_sembako.py
+++ rm_base_rnd/models/warung_sembako.py
@@ -7,6 +7,10 @@ class WarungSembako(models.Model):
     
     partner_id = fields.Many2one('res.partner', string='Pembeli')
     date = fields.Date('Tanggal', default=fields.Date.today())
+    tipe_sembako = fields.Selection([
+        ('subsidi', 'Subsidi'),
+        ('general', 'General')
+    ], string='Tipe Sembako')
     # KEY
     sembako_line = fields.One2many('barang.sembako', 'sembako_id', string='Komponen')
 
@@ -21,8 +25,21 @@ class BarangSembako(models.Model):
     standard_price = fields.Monetary('Harga')
     qty = fields.Float('Kuantiti')
     total = fields.Float('Total')
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    # REL
+    tipe_sembako_rel = fields.Selection([
+        ('subsidi', 'Subsidi'),
+        ('general', 'General')
+    ], string='Tipe Sembako', related='sembako_id.tipe_sembako')
 
-    @api.onchange('standard_price')
+    @api.onchange('tipe_sembako_rel')
+    def _onchange_prod_id(self):
+        if self.tipe_sembako_rel == 'general':
+            return {"domain": {'product_id':[]}}
+        else:
+            return {"domain": {'product_id':[('product_subsidi', '=', True)]}}
+
+    @api.onchange('standard_price','qty')
     def _onchange_standard_price(self):
         self.qty == 0
         if self.standard_price:
